6d8693fa7c48cec3a3ca52613f592066d9d95908
Ugur Tigu <ugurtigu@gmail.com>
2020-10-03 18:14:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let themeDots = document.getElementsByClassName('theme-dot')
+
+let theme = localStorage.getItem('theme')
+
+if(theme === null){
+    setTheme('blue')
+} else{
+    setTheme(theme)
+}
+
+for (let index = 0; themeDots.length > index; index++) {
+    themeDots[index].addEventListener('click', function () {
+        let mode = this.dataset.mode;
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    if (mode === 'light') {
+        document.getElementById('theme-style').href = 'default.css'
+    }
+    if (mode === 'blue') {
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+    if (mode === 'green') {
+        document.getElementById('theme-style').href = 'green.css'
+    }
+    if (mode === 'purple') {
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+
+}
